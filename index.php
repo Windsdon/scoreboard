4ff3f229ffd932c01acd6541365d32eb2b5b6f28
Windsdon <jpfm2210@gmail.com>
2013-07-13 09:26:23 -0300

    Added Style testing

--- index.php
+++ index.php
@@ -1,7 +1,4 @@
 <?php
-include_once "functions/startup.php";
-include_once "functions/db_functions.php";
-include_once "functions/error_logger.php";
 include_once "functions/system.php";
 
 new ScoreboardSystem();
